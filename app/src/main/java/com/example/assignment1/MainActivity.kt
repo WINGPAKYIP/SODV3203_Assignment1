dd990af7774701f6c119ced8a51f7a9a9b849f0c
WINGPAKYIP <137473986+WINGPAKYIP@users.noreply.github.com>
2024-02-02 00:23:55 -0700

    Initial commit

---
+++ app/src/main/java/com/example/assignment1/MainActivity.kt
@@ -0,0 +1,111 @@
+package com.example.assignment1
+
+import android.graphics.drawable.Icon
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Phone
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.assignment1.ui.theme.Assignment1Theme
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Assignment1Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    BusinessCard()
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun BusinessCard(modifier: Modifier = Modifier) {
+    val androidGreen = Color(0xFFA4C639)
+    val darkGreen = Color (0xFF00574B)
+    Column(
+        modifier = modifier
+            .fillMaxSize()  // Make sure the Column fills the entire size
+            .background(color = androidGreen),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.profilepic),
+            contentDescription = null, // Content description for accessibility
+            modifier = Modifier.size(100.dp)
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = "Wing Pak Yip",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(
+            text = "Student",
+            fontSize = 16.sp
+        )
+        Spacer(modifier = Modifier.height(72.dp))
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Icon(Icons.Default.Phone, contentDescription = "Phone Icon", tint = darkGreen)
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(
+                text = "+1 123-456-7890",
+                fontSize = 14.sp
+            )
+        }
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Icon(Icons.Default.Person, contentDescription = "Social Media Icon", tint = darkGreen)
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(
+                text = "@socialmediahandle",
+                fontSize = 14.sp
+            )
+        }
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Icon(Icons.Default.Email, contentDescription = "Email Icon", tint = darkGreen)
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(
+                text = "example@example.com",
+                fontSize = 14.sp
+            )
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun BusinessCardPreview() {
+    Assignment1Theme {
+        BusinessCard()
+    }
+}
